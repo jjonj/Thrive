6f22774ad9f717f890b99bda68b50feeacca7485
Dani Ferri Tomas <daniferrito2@hotmail.com>
2013-05-24 22:09:59 +0200

    Adding linux script.

--- mingw_setup/MyGUI/install.sh
+++ mingw_setup/MyGUI/install.sh
@@ -4,17 +4,13 @@
 
 MINGW_ENV=$1
 
-BUILD_TYPE=Debug
+WORKING_DIR=$MINGW_ENV/temp/MyGUI 
 
-WORKING_DIR=$MINGW_ENV/temp/ogre
+REMOTE_DIR="http://sourceforge.net/projects/my-gui/files/MyGUI/MyGUI_3.2.0/"
 
-REMOTE_DIR="http://downloads.sourceforge.net/project/ogre/ogre/1.8/1.8.1"
-
-ARCHIVE="ogre_src_v1-8-1.exe"
+ARCHIVE="MyGUI_3.2.0.zip"
 
 TOOLCHAIN_FILE=$MINGW_ENV/cmake/toolchain.cmake
-
-OGRE_SRC_DIR=$WORKING_DIR/ogre_src_v1-8-1
 
 mkdir -p $WORKING_DIR
 
@@ -25,10 +21,10 @@ mkdir -p $WORKING_DIR
 if [ -e $WORKING_DIR/$ARCHIVE ]; then
     echo "OGRE archive file found, skipping download. If you want to redownload it, please delete ${WORKING_DIR}/${ARCHIVE}."
 else
-    echo "Downloading OGRE"
+    echo "Downloading MyGUI"
     wget -O $WORKING_DIR/$ARCHIVE $REMOTE_DIR/$ARCHIVE
     if [ $? -gt 0 ]; then
-        echo "Error downloading OGRE archive. Aborting."
+        echo "Error downloading MyGUI archive. Aborting."
         rm $WORKING_DIR/$ARCHIVE
         exit 1
     fi
@@ -42,7 +38,7 @@ fi
 # untar the sources
 7za x -y -o$WORKING_DIR $WORKING_DIR/$ARCHIVE
 if [ $? -ne 0 ]; then
-    echo "Could not unpack OGRE If the archive is corrupted, delete 
+    echo "Could not unpack MyGUI If the archive is corrupted, delete 
 
        $WORKING_DIR/$ARCHIVE 
 
@@ -54,17 +50,13 @@ mkdir -p $WORKING_DIR/build
 cd $WORKING_DIR/build
 cmake \
     -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
-    -DOGRE_DEPENDENCIES_DIR=$MINGW_ENV/install \
-    -DCMAKE_INSTALL_PREFIX=$MINGW_ENV/OgreSDK \
-    -DOGRE_CONFIG_THREADS=1 \
-    -DOGRE_USE_BOOST=ON \
-    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DOGRE_BUILD_RENDERSYSTEM_D3D9=OFF \
-    -DDirectX_DXERR_LIBRARY=$MINGW_ENV/x86_64-mingw32/lib/libdxerr9.a \
-    $WORKING_DIR/ogre_src_v1-8-1
+    -DOGRE_SOURCE=$MINGW_ENV/OgreSDK \
+    -DBoost_DIR=$MINGW_ENV/install \
+    -DBOOST_ROOT=$MINGW_ENV/install \
+    -DCMAKE_INSTALL_PREFIX=$MINGW_ENV/install \
+    -DMYGUI_BUILD_DEMOS=false \
+    -DMYGUI_BUILD_TOOLS=false \
+    -DMYGUI_DEPENDENCIES_DIR=$MINGW_ENV/install \
+    $WORKING_DIR/MyGUI_3.2.0
 make -j4 && make install
-
-# Fix case sensitive directory names
-rsync -a $MINGW_ENV/OgreSDK/lib/$BUILD_TYPE/* $MINGW_ENV/OgreSDK/lib/`echo ${BUILD_TYPE,,}`
-rsync -a $MINGW_ENV/OgreSDK/bin/$BUILD_TYPE/* $MINGW_ENV/OgreSDK/bin/`echo ${BUILD_TYPE,,}`
 
